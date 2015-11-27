ad07eda3aa5186bc0aa241ea9d940e4b9b310f35
Brennan Crispin <brennan@iruleathome.com>
2015-11-27 12:15:00 -0500

    Added search and sort functions

---
+++ dateTypeView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Brennan Crispin on 11/27/15.
+//
+
+#include "dateTypeView.h"
---
+++ dateTypeView.h
@@ -0,0 +1,14 @@
+//
+// Created by Brennan Crispin on 11/27/15.
+//
+
+#ifndef PROJECT2_DATETYPEVIEW_H
+#define PROJECT2_DATETYPEVIEW_H
+
+
+class dateTypeView {
+
+};
+
+
+#endif //PROJECT2_DATETYPEVIEW_H
